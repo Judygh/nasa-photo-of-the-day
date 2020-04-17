30a5ebbc0dfeefe4768f3da490da4d9c4bebf332
Judy Ghashim <judyghashim@Judys-MacBook-Pro.local>
2020-04-16 22:54:55 -0500

    adding some features

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
+import { Card, CardImg, CardText, CardBody, CardTitle, Spinner } from "reactstrap";
+import { Container,Jumbotron } from "reactstrap";
+
+import styled from "styled-components"
+
+import ListGroupItemHeading from "reactstrap/lib/ListGroupItemHeading";
+import CardHeader from "reactstrap/lib/CardHeader";
 
 function App() {
   //   return (
@@ -13,6 +20,12 @@ function App() {
   //   );
   // }
 
+  // const MidCard = styled.div`
+
+  // background: red;
+  // color:red;
+
+  // `;
   const [nasaImg, setNasaImg] = useState({ data: [] });
   console.log("Something is Wrong", nasaImg.data);
   const nasa = nasaImg.data;
@@ -21,18 +34,31 @@ function App() {
     const fetchImg = () => {
       axios
         .get(
-          `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`
+          `https://api.nasa.gov/planetary/apod?api_key=XUaWfJMY6fhHXRZ15leZc3QOpkNV2XYBfdyS1IvJ`
         )
         .then((res) => setNasaImg(res));
     };
     fetchImg();
   }, []);
+
   return (
-    <div className="App">
-      <h1 className="heading">NASA Pic of the day!</h1>
-      <img src={nasa.url} alt='Nasa Pic of the Day' key={nasaImg} />
-      <p className="explanation">{nasa.explanation}</p>
-    </div>
+    // <div className="App">
+    //   {/* <h1 className="heading">NASA Pic of the day!</h1>
+    //   <img src={nasa.url} alt='Nasa Pic of the Day' key={nasaImg} />
+    //   <p className="explanation">{nasa.explanation}</p> */}
+    // </div>
+    // <col xs ="1" md =""
+  
+    <Container>
+      
+      <Card>
+        <CardTitle>Nasa pic of the day! </CardTitle>
+        <CardImg width="50%" src={nasa.url} alt="Nasa Pic of the Day" />
+        <CardText>{nasa.explanation} <Spinner type="grow" color="primary" /> </CardText>
+        
+      </Card>
+     
+    </Container>
   );
 }
 
