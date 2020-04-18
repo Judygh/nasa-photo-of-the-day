e4a1ab3f75432363642286cf07f4e776c29f540e
Judy Ghashim <judyghashim@Judys-MacBook-Pro.local>
2020-04-17 20:23:27 -0500

    adding some styling in it

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
-import { Card, CardImg, CardText, CardBody, CardTitle, Spinner } from "reactstrap";
-import { Container,Jumbotron } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Spinner,
+} from "reactstrap";
+import { Container, Jumbotron } from "reactstrap";
 
-import styled from "styled-components"
-
-import ListGroupItemHeading from "reactstrap/lib/ListGroupItemHeading";
-import CardHeader from "reactstrap/lib/CardHeader";
+import styled from "styled-components";
 
 function App() {
-  //   return (
-  //     <div className="App">
-  //       <p>
-  //         Read through the instructions in the README.md file to build your NASA
-  //         app! Have fun 🚀!
-  //       </p>
-  //     </div>
-  //   );
-  // }
 
-  // const MidCard = styled.div`
-
-  // background: red;
-  // color:red;
-
-  // `;
+ 
+  
   const [nasaImg, setNasaImg] = useState({ data: [] });
   console.log("Something is Wrong", nasaImg.data);
   const nasa = nasaImg.data;
@@ -41,24 +32,27 @@ function App() {
     fetchImg();
   }, []);
 
+  const MidCard = styled.div`
+  margin: auto;
+  padding-top: 20px;
+  padding-button: 20px;
+  background-color: #716f9d;
+`;
+const MidCard = styled.card-text `
+font-size: 1.5em;
+text-align: center;
+color: palevioletred;
+`;
   return (
-    // <div className="App">
-    //   {/* <h1 className="heading">NASA Pic of the day!</h1>
-    //   <img src={nasa.url} alt='Nasa Pic of the Day' key={nasaImg} />
-    //   <p className="explanation">{nasa.explanation}</p> */}
-    // </div>
-    // <col xs ="1" md =""
-  
-    <Container>
-      
-      <Card>
+    <MidCard>
+      <Container>
         <CardTitle>Nasa pic of the day! </CardTitle>
-        <CardImg width="50%" src={nasa.url} alt="Nasa Pic of the Day" />
-        <CardText>{nasa.explanation} <Spinner type="grow" color="primary" /> </CardText>
-        
-      </Card>
-     
-    </Container>
+        <CardImg src={nasa.url} alt="Nasa Pic of the Day" />
+        <CardText>
+          {nasa.explanation} <Spinner type="grow" color="primary" />{" "}
+        </CardText>
+      </Container>
+    </MidCard>
   );
 }
 
---
+++ src/extra.js
@@ -0,0 +1,22 @@
+  //   return (
+  //     <div className="App">
+  //       <p>
+  //         Read through the instructions in the README.md file to build your NASA
+  //         app! Have fun 🚀!
+  //       </p>
+  //     </div>
+  //   );
+  // }
+
+
+  import ListGroupItemHeading from "reactstrap/lib/ListGroupItemHeading";
+import CardHeader from "reactstrap/lib/CardHeader";
+
+
+
+// <div className="App">
+    //   {/* <h1 className="heading">NASA Pic of the day!</h1>
+    //   <img src={nasa.url} alt='Nasa Pic of the Day' key={nasaImg} />
+    //   <p className="explanation">{nasa.explanation}</p> */}
+    // </div>
+    // <col xs ="1" md =""
